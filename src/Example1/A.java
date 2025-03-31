777c9eb675b5644beddd40853e57eb19339b358f
sayam <ssdmmishra456@gmail.com>
2025-03-31 16:52:45 +0530

    first commit

---
+++ src/Example1/A.java
@@ -0,0 +1,13 @@
+package Example1;
+
+public class A {
+	public static void main(String[] args) {
+		A a1= new A();
+		System.out.println(a1);
+		A a2 = new A ();
+		System.out.println(a2);
+		A a3 = new A();
+		System.out.println(a3);
+	}
+
+}
